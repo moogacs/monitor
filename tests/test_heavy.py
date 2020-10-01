a27e856b3b50ca2383dd168476639ca1226e8e7a
moogacs <contact@m-mooga.com>
2020-10-02 00:47:46 +0200

    add: test case to check in 2 cycle messges count is double

--- tests/test_heavy.py
+++ tests/test_heavy.py
@@ -66,5 +66,30 @@ class test_heavy(unittest.TestCase):
         #send messages equals total urls count in 1 cycle
         self.assertEqual(prod.get_message_count(), len(monitors))
 
+    def test_validate_number_in_interval_double(self):
+        prod, cons = app.run(Config.K_MONITOR_TEST_TOPIC,
+                            Config.PS_DATABASE_NAME,
+                            Config.PS_TEST_WEBSITE_TABLE_NAME,
+                            "tests/t_monitor_heavy_test.yml")
+
+        interval = File.read_time_interval("tests/t_monitor_heavy_test.yml")
+
+        time.sleep(interval*2)
+
+        app.stop_monitor(prod, cons)
+
+        admin_client = KafkaAdminClient(bootstrap_servers=[Config.K_HOST+':'+Config.K_PORT],
+                                        security_protocol=Config.K_SECURITY_PROTOCOL,
+                                        ssl_cafile=Config.K_SSL_CAT_FILE,
+                                        ssl_certfile=Config.K_SSL_CERT_FILE,
+                                        ssl_keyfile=Config.K_SSL_KEY_FILE)
+
+        admin_client.delete_topics([Config.K_MONITOR_TEST_TOPIC])
+
+        monitors = File.read_monitors("tests/t_monitor_heavy_test.yml")
+
+        #send messages equals total urls count in 2 cycle is double the urls size
+        self.assertEqual(prod.get_message_count(), len(monitors)*2)
+
 if __name__ == '__main__': 
     unittest.main() 
